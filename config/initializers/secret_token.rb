ddef9746a2fe98b99766d045e17f00d313a6e688
santaux <santaux@gmail.com>
2011-10-03 14:58:10 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tourism::Application.config.secret_token = '7b50ff123f9a4d6f737cc6b079c4a886083a6d9bdef3137dbcf776e8fac7d1c406ce2842cff42ce045b2be05daa4fd3c1fc492fcc2de9388bf4b834dff642912'

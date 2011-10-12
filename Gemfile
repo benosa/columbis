268da7e57a0a994f677f11f726ef6bce7fbf2d54
santaux <santaux@gmail.com>
2011-10-12 19:30:01 +0400

    [refs #16] Add capistrano and deploy.rb

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ end
 group :development do
   gem 'mongrel', '>= 1.2.0.pre2'
   gem 'nifty-generators'
+  gem 'capistrano'
 end
 
 group :test do
---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+require 'bundler/capistrano'
+
+set :application, 'tourism'
+set :repository,  "git@devmen.unfuddle.com:devmen/tourism.git"
+
+set :scm, :git
+
+role :app, "devmen.com"
+role :web, "devmen.com"
+role :db,  "devmen.com", :primary => true
+
+set :deploy_to, "/opt/apps/tourism"
+set :user, "deploy"
+set :use_sudo, false
+set :rails_env, "production"
+set :shared_host, "tourism.devmen.com"
+
+after "deploy:update_code", "deploy:migrate"
+after "deploy:migrate", "deploy:seed"
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end

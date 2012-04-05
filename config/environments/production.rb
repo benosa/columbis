680fd5373568faaffc753792011f9e8856a5af37
terraplane <stratonaut@gmail.com>
2012-04-05 14:03:00 +0300

    setup default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,6 @@ Tourism::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'tourism.devmen.com' }
 end

cf6562b9191e2b8171b3cfcaf30b4826b4e7383b
terraplane <stratonaut@gmail.com>
2012-04-09 12:20:13 +0300

    mailer conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,10 +59,10 @@ Tourism::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.default_url_options = { :host => 'tourism.devmen.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
+#  config.action_mailer.delivery_method = :smtp
+#  config.action_mailer.perform_deliveries = true
+#  config.action_mailer.raise_delivery_errors = false
+#  config.action_mailer.default :charset => "utf-8"
 
 
   config.middleware.use ExceptionNotifier,

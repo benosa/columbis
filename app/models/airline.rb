8f725ad6bc67dcef548832e7347742bb75d0bfa0
terraplane <stratonaut@gmail.com>
2012-04-09 16:13:31 +0300

    airlines name validations

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,6 +1,8 @@
 class Airline < ActiveRecord::Base
   attr_accessible :name
   attr_protected :company_id
+
   belongs_to :company
+  validates_presence_of :name
   validates_uniqueness_of :name
 end

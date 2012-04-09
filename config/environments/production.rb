1a90571cdc34b98ac914c77e5f7cf5c870d44ba2
terraplane <stratonaut@gmail.com>
2012-04-09 14:08:54 +0300

    mailer reconf for gmail using

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,12 +58,20 @@ Tourism::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'tourism.devmen.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
-
+    config.action_mailer.default_url_options = { :host => 'tourism.devmen.com' }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.default :charset => "utf-8"
+    config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "tourism.devmen.com",
+      :authentication => "plain",
+      :user_name => "mailer.devmen.com@gmail.com",
+      :password => "mailer.devmen",
+      :enable_starttls_auto => true
+    }
 
   config.middleware.use ExceptionNotifier,
     :email_prefix => '[Tourism] ',

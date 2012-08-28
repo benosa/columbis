fda96366f1ef28d6847fa85102744a74f3a209ca
zaMmer <alexzammer@gmail.com>
2012-08-28 23:26:35 +0300

    Notice about empty fields in printing forms

--- app/models/printer.rb
+++ app/models/printer.rb
@@ -14,20 +14,28 @@ class Printer < ActiveRecord::Base
 
   def prepare_template(fields, collections)
     @text = File.read(template.path)
+    @empty_fields = []    
 
     setup_collections(collections)
 
     fields.each do |key, value|
       value ||= ''
-      @text.gsub!(/\#\{#{key.mb_chars.upcase}\}/, (value.respond_to?(:strftime) ? value.strftime('%d/%m/%Y') : value.to_s))
+      upkey = key.mb_chars.upcase
+      @text.gsub!(/\#\{#{upkey}\}/) do
+        @empty_fields << key if (value == '' and !unrequired_fields.include?(upkey))        
+        (value.respond_to?(:strftime) ? value.strftime('%d/%m/%Y') : value.to_s)
+      end
     end
-
+    @empty_fields.uniq! 
+    @text.gsub!(/\#\{ПУСТЫЕ_ПОЛЯ\}/, empty_fields_message)
+    
     @text
   end
 
   private
 
   def setup_collections(collections)
+    @empty_collection_fields = {}
     collections.each do |key, value|
       collection_name = key.mb_chars.upcase.to_s        
       @text.scan(/(\%\{#{collection_name}\}(.+?)\{#{collection_name}\}\%)/m).each do |matches|
@@ -43,7 +51,13 @@ class Printer < ActiveRecord::Base
             next if collection_key == :collection
             collection_field ||= ''.to_sym
             field = ob.try(collection_field)
-            row.gsub!(/\#\{#{collection_key.mb_chars.upcase}\}/, (field.respond_to?(:strftime) ? field.strftime('%d/%m/%Y') : field.to_s))
+            upkey = collection_key.mb_chars.upcase            
+            row.gsub!(/\#\{#{upkey}\}/, (field.respond_to?(:strftime) ? field.strftime('%d/%m/%Y') : field.to_s))
+            if (field.to_s == '' and !unrequired_fields.include?(upkey)) 
+              @empty_collection_fields[key] = [] if @empty_collection_fields[key].nil?
+              @empty_collection_fields[key][index] = [] if @empty_collection_fields[key][index].nil?
+              @empty_collection_fields[key][index] << collection_key
+            end
           end
           # Computed expressions
           row.gsub!(/\=\{(.+?)\}/) do |m|
@@ -57,7 +71,28 @@ class Printer < ActiveRecord::Base
           index += 1
         end
         @text.gsub!(match, result)
-      end
+      end      
     end
+  end
+
+  def empty_fields_message
+    return '' if @empty_fields.length == 0 and @empty_collection_fields.length == 0
+    message = "В документе присутствуют незаполненные поля.\n"
+    message += "Список полей: #{@empty_fields.join(', ')}\n" if @empty_fields.length > 0
+    Rails.logger.debug "@empty_collection_fields: #{@empty_collection_fields.inspect}"
+    @empty_collection_fields.each do |ckey, rows|
+      message += "#{ckey}:\n"
+      rows.each_with_index { |fields, index| message += "#{index} - #{fields.join(', ')}\n" }
+    end
+    message.gsub(/\n/, '<br/>')
+  end
+
+  def unrequired_fields
+    return @unrequired_fields if @unrequired_fields.present?
+    @unrequired_fields = []
+    s = @text.index('%{НЕОБЯЗАТЕЛЬНЫЕ_ПОЛЯ}').to_i + '%{НЕОБЯЗАТЕЛЬНЫЕ_ПОЛЯ}'.length
+    e = @text.index('{НЕОБЯЗАТЕЛЬНЫЕ_ПОЛЯ}%').to_i    
+    @unrequired_fields += @text[s, e - s].strip.gsub(/[\s\n]/, '').split(',') if (s < e)
+    @unrequired_fields    
   end
 end

c5acb88672ed8d146dc0a2be90d487a25475c0ec
zaMmer <alexzammer@gmail.com>
2012-08-29 11:36:53 +0300

    Fix doc templates download

--- app/controllers/dashboard/printers_controller.rb
+++ app/controllers/dashboard/printers_controller.rb
@@ -2,13 +2,13 @@ class Dashboard::PrintersController < ApplicationController
   load_and_authorize_resource
 
   def download
-    # @company = current_company unless @company
-    # if params[:printer]
-    #   @printer = @company.printers.find(params[:printer]).first
-    #   send_file @printer.template.path, :filename => @printer.template.file.identifier
-    # else
-    #   render :404
-    # end
-    send_file @printer.template.path, :filename => @printer.template.file.identifier
+    @company = current_company unless @company
+    if params[:template]
+      @printer = @company.printers.find(params[:template])
+      send_file @printer.template.path, :filename => @printer.template.file.identifier
+    else
+      render :'404'
+    end
+    #send_file @printer.template.path, :filename => @printer.template.file.identifier
   end  
 end

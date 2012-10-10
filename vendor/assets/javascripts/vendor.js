8360bde580ef9826be20d06bdb3b231d078e0226
zaMmer <alexzammer@gmail.com>
2012-10-10 14:16:03 +0300

    Add editable select functionality to ikSelect jquery plugin

--- vendor/assets/javascripts/vendor.js
+++ vendor/assets/javascripts/vendor.js
@@ -1,7 +1,7 @@
 //= require autocomplete-rails
 //= require jquery-ui.slider_access
 //= require jquery.editable-select
-//= require jquery.ikSelect.min
+//= require jquery.ikSelect
 //= require jquery.ui.datepicker
 //= require jquery.ui.datepicker-ru
 //= require jquery.ui.slider

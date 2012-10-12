4ae6037ee4a259993bd2e78a51117af83b5dc878
zaMmer <alexzammer@gmail.com>
2012-10-12 19:52:28 +0300

    Add highlighted options to custom form builder

--- app/helpers/tourism_form_builder.rb
+++ app/helpers/tourism_form_builder.rb
@@ -1,8 +1,25 @@
 class TourismFormBuilder < ActionView::Helpers::FormBuilder
+  def initialize(*args)
+    super(*args)
+    @highlighted_options ||= {
+      :only => @options[:highlight_only].present? ? @options[:highlight_only].to_a : nil,
+      :except => @options[:highlight_except].present? ? @options[:highlight_except].to_a : nil
+    }
+    Rails.logger.debug "@highlighted_options: #{@highlighted_options.to_yaml}"
+end
+
   def self.create_wrapped_field(method_name)
     define_method(method_name) do |name, *args|
       options = args.dup.extract_options!
-      if !options[:not_highlighted] and @object.class.validators_on(name).any? { |v| v.instance_of? ActiveModel::Validations::PresenceValidator }
+      if options[:highlighted] or options[:not_highlighted]
+        highlight = options[:highlighted] and !options[:not_highlighted]
+      else
+        highlight = @object.class.validators_on(name).any? { |v| v.instance_of? ActiveModel::Validations::PresenceValidator }
+        highlight &&= !@highlighted_options[:except].include?(name) if highlight and @highlighted_options[:except].present?
+        highlight &&= @highlighted_options[:only].include?(name) if @highlighted_options[:only].present?
+      end
+
+      if highlight
         @template.content_tag('div', super(name, *args), :class => "highlighted")
       else
         super(name, *args)

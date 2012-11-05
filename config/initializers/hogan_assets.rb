72e1e26c73579a1fce2b2b68daf78d15ef0b3ec8
zaMmer <alexzammer@gmail.com>
2012-11-05 03:38:56 +0200

    Fix hogan asset initializer

--- config/initializers/hogan_assets.rb
+++ config/initializers/hogan_assets.rb
@@ -1,5 +1,7 @@
-HoganAssets::Config.configure do |config|
-  config.lambda_support = true
-  config.template_namespace = 'JST'
-  config.path_prefix = 'templates'
+if Object.const_defined?('HoganAssets') # avoiding uninitialized constant error in rake tasks
+  HoganAssets::Config.configure do |config|
+    config.lambda_support = true
+    config.template_namespace = 'JST'
+    config.path_prefix = 'templates'
+  end
 end

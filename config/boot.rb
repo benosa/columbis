19596e30372d65308fee81fcb5fb6f2ad95a5ce8
zaMmer <alexzammer@gmail.com>
2013-01-29 14:28:49 +0200

    Workaround for prych yaml parser

--- config/boot.rb
+++ config/boot.rb
@@ -5,5 +5,7 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# Workaround with psych yaml parser
 require 'yaml'
 YAML::ENGINE.yamler = 'syck'

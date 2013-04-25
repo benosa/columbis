07090d626615613444c10ac1ab617ba3cfd6d734
zaMmer <alexzammer@gmail.com>
2013-04-25 18:59:45 +0300

    Add script and cron job for transfer bazoved database to columbis

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,3 +3,7 @@ set :output, { :standart => 'log/cron.log', :error => 'log/cron.log' }
 every 1.day, :at => '1:17am', :roles => [:app] do
   rake 'claims:expire_active_cache'
 end
+
+every '25 5-17/3,23 * * *' do
+  command 'script/transfer'
+end

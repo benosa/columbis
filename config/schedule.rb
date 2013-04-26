add569561e2050f0a0cb9845d6f83fb6109c29b9
zaMmer <alexzammer@gmail.com>
2013-04-26 19:50:44 +0300

    Improve transfer script

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,9 @@
-set :output, { :standart => 'log/cron.log', :error => 'log/cron.log' }
+set :output, File.expand_path('../../log/cron.log', __FILE__)
 
 every 1.day, :at => '1:17am', :roles => [:app] do
   rake 'claims:expire_active_cache'
 end
 
-every '25 5-17/3,23 * * *' do
+every '25 5-17/2,23 * * *' do
   command File.expand_path('../../script/transfer', __FILE__)
 end

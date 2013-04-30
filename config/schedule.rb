1be5f4cfddd34be7dd8e269073d9f658049a46a7
zaMmer <alexzammer@gmail.com>
2013-04-30 12:39:34 +0300

    Use proper rvm settings for cron jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,10 @@
 set :output, File.expand_path('../../log/cron.log', __FILE__)
+set :rvm_ruby_string, '1.9.3@tourism'
+
+job_type :rake_with_rvm, 'rvm use :rvm_ruby_string && cd :path && RAILS_ENV=:environment bundle exec rake :task --trace :output'
 
 every 1.day, :at => '1:17am', :roles => [:app] do
-  rake 'claims:expire_active_cache'
+  rake_with_rvm 'claims:expire_active_cache'
 end
 
 every '25 5-17/2,23 * * *' do

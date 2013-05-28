7346c078802b79b88729bb143755deedf12ef70a
zaMmer <alexzammer@gmail.com>
2013-05-28 17:21:54 +0300

    Fix models in migrations

--- db/migrate/20130328114121_set_canceled_in_payments.rb
+++ db/migrate/20130328114121_set_canceled_in_payments.rb
@@ -1,11 +1,16 @@
 class SetCanceledInPayments < ActiveRecord::Migration
-  class Claim < ActiveRecord::Base; end
-  class Payment < ActiveRecord::Base; end
+  class Claim < ActiveRecord::Base
+    has_many :payments_in, :class_name => 'Payment', :conditions => { :recipient_type => 'Company' }
+    has_many :payments_out, :class_name => 'Payment', :conditions => { :payer_type => 'Company' }
+  end
+  class Payment < ActiveRecord::Base
+    belongs_to :claim
+  end
 
   def up
     Claim.select(:id).where(canceled: true).includes(:payments_in, :payments_out).find_each(:batch_size => 500) do |claim|
       payment_ids = claim.payments_in.map(&:id)
-      payment_ids += claim.payments_in.map(&:id)
+      payment_ids += claim.payments_out.map(&:id)
       payment_ids = payment_ids.uniq.compact
       Payment.update_all({ canceled: true }, { id:  payment_ids}) unless payment_ids.empty?
     end
@@ -14,7 +19,7 @@ class SetCanceledInPayments < ActiveRecord::Migration
   def down
     Claim.select(:id).where(canceled: true).includes(:payments_in, :payments_out).find_each(:batch_size => 500) do |claim|
       payment_ids = claim.payments_in.map(&:id)
-      payment_ids += claim.payments_in.map(&:id)
+      payment_ids += claim.payments_out.map(&:id)
       payment_ids = payment_ids.uniq.compact
       Payment.update_all({ canceled: false }, { id:  payment_ids}) unless payment_ids.empty?
     end

9422b514e2522bbf22dedac3b894b3567ce8fb41
TOTOIIIUr <totoiiie4ka@gmail.com>
2013-06-11 00:27:47 +0400

    code refactoring

--- app/assets/javascripts/pages/clock.js
+++ app/assets/javascripts/pages/clock.js
@@ -1,12 +1,7 @@
 $(document).ready(function(){
 	var elem = $("p#clock");
 	sync_clock(elem);
-	var sync_id = setInterval(function() {
-		if (elem.text() != "") {
-			local_timer(elem);
-			clearInterval(sync_id);
-		}
-	}, 1000);
+	local_timer(elem);
 	setInterval(function() { sync_clock(elem); }, 3600000)
 });
 
@@ -18,10 +13,6 @@ function local_timer(elem) {
 	setInterval(function() {
 		var time = new Date("01/01/2013 " + elem.text());
 		time.setSeconds(time.getSeconds()+1);
-		var OH = "", OM = "", OS = "";
-		if (time.getHours() < 10) { OH = "0";	}
-		if (time.getMinutes() < 10) {	OM = "0";	}
-		if (time.getSeconds() < 10) {	OS = "0";	}
-		elem.text(OH + time.getHours() + ":" + OM + time.getMinutes() + ":" + OS + time.getSeconds());
+		elem.text(Globalize.format(time, 'HH:mm:ss'));
 	}, 1000)
 }

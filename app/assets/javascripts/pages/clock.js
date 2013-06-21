fc67c56a7d0745e3cd67340c2eb814d586bb1478
TOTOIIIUr <totoiiie4ka@gmail.com>
2013-06-21 12:14:44 +0400

    new clock realization with local time and sync

--- app/assets/javascripts/pages/clock.js
+++ app/assets/javascripts/pages/clock.js
@@ -1,18 +1,41 @@
 $(document).ready(function(){
 	var elem = $("p#clock");
-	sync_clock(elem);
-	local_timer(elem);
-	setInterval(function() { sync_clock(elem); }, 3600000)
+	// start_time needs to take delay of ajax request and local time
+	var start_time = new Date().getTime();
+	elem.load({ url: '/set_time', success: local_timer(elem, start_time)});
 });
 
-function sync_clock(elem) {
-	setTimeout(function() {elem.load('/set_time');}, 1000)
+function local_timer(elem, start_time) {
+	var local_time = new Date().getTime();
+	var server_time = (new Date("01/01/2013 " + elem.text())).getTime();
+	var delay = local_time - start_time;
+	var delta = local_time - (server_time - (delay / 2));
+	var tick = 0;
+	var is_syncing = false
+
+	interval_id = setInterval(function() {
+		tick ++;
+		// Set time to form
+		elem.text(Globalize.format(new Date(new Date().getTime() - delta), 'HH:mm:ss'));
+		// begin sync with server every 1 hour
+		if (tick > (1000 * 60 * 60)) {
+			if(is_syncing == false) {
+				elem.load({
+					url: '/set_time',
+					success: new_sync(elem, interval_id)
+				});	
+			}
+			is_syncing = true;
+		}
+		// end sync with server every 1 hour
+	}, 1000);
 }
 
-function local_timer(elem) {
-	setInterval(function() {
-		var time = new Date("01/01/2013 " + elem.text());
-		time.setSeconds(time.getSeconds()+1);
-		elem.text(Globalize.format(time, 'HH:mm:ss'));
-	}, 1000)
+function new_sync(elem, interval_id) {
+	// clear previous interval
+	clearInterval(interval_id);
+	var start_time = new Date().getTime();
+	// cyclic call one method from another here does not lead to overflows since this call occurs once every hour
+	local_timer(elem, start_time);
 }
+

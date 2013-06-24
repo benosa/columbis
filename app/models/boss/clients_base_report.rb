c21b6718821987e154aee464d4c8519b7675872e
TOTOIIIUr <totoiiie4ka@gmail.com>
2013-06-24 20:58:07 +0400

    52.7. ABC analysis

--- app/models/boss/clients_base_report.rb
+++ app/models/boss/clients_base_report.rb
@@ -2,6 +2,33 @@ module Boss
   class ClientsBaseReport < Report
     arel_tables :payments
     available_results :count, :amount
+    attribute :intervals
+
+    attr_accessible :intervals
+
+    def initialize(attributes = nil, options = {})
+      super
+
+      # Default intervals
+      unless intervals
+        self.intervals = {
+          :amount => [ {procent: 0.80, name: "80%"},
+                       {procent: 0.95, name: "15%"},
+                       {procent: 1, name: "05%"}
+                     ],
+          :payer_id => [ {procent: 0.50, name: "50%"},
+                         {procent: 0.80, name: "30%"},
+                         {procent: 1, name: "20%"}
+                       ]
+        }
+      end
+      
+      intervals.each do |interval|
+        interval[1].each do |i|
+          i[:name] = I18n.t(".clientsbase_report." + interval[0].to_s, value: i[:name])
+        end
+      end
+    end
 
     def prepare(options = {})
       @results[:count]  = build_result(query: count_query,  typecast: {count: :to_i})
@@ -9,7 +36,16 @@ module Boss
       self
     end
 
-    def bar_settings(factor, data)      
+    def interval_field(column, foo, sort)
+      expr = ''
+      intervals[column].slice(0..(intervals[column].length-2)).each do |interval|
+        expr += "WHEN #{foo}(#{column}) over(order by amount #{sort} rows unbounded preceding) <= #{interval[:procent]} * #{foo}(#{column}) over() THEN '#{interval[:name]}' "
+      end
+      expr += "ELSE '#{intervals[column].last[:name]}'"
+      "(CASE #{expr} END)"
+    end
+
+    def bar_settings(factor, data)
       if factor == :amount
         title = "#{I18n.t('report.amount')}, #{I18n.t('rur')}"
         ytitle = I18n.t('rur')
@@ -70,37 +106,29 @@ module Boss
           .group(payments[:payer_id])
           .as("t")
       end
-      
+
       def count_query
         query = payments.project(
             "payer_id",
-            "(CASE
-              WHEN sum(amount) over(order by amount desc rows unbounded preceding) <= 0.8 * sum(amount) over() THEN '80% выручки'
-              WHEN sum(amount) over(order by amount desc rows unbounded preceding) <= 0.95 * sum(amount) over() THEN '15% выручки'
-              ELSE '05% выручки'
-            END) as name",
+            "#{interval_field(:amount, "sum", "desc")} as name",
             )
           .from(base_query)
           .as("count")
-        
+
         payments.project( "count(payer_id)", "name" )
           .from(query)
           .group("name")
           .order("name")
       end
-      
+
       def amount_query
         query = payments.project(
             "amount",
-            "(CASE
-              WHEN count(payer_id) over(order by amount asc rows unbounded preceding) <= 0.5 * count(payer_id) over() THEN '50% клиентов'
-              WHEN count(payer_id) over(order by amount asc rows unbounded preceding) <= 0.8 * count(payer_id) over() THEN '30% клиентов'
-              ELSE '20% клиентов'
-            END) as name",
+            "#{interval_field(:payer_id, "count", "asc")} as name",
             )
           .from(base_query)
           .as("amount")
-        
+
         payments.project( "sum(amount) as amount", "name" )
           .from(query)
           .group("name")

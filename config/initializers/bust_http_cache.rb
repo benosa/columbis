320fe7fbbeef753425ef3bbe459a024588ce2450
zaMmer <alexzammer@gmail.com>
2013-07-18 01:35:41 +0300

    [#63] Fix bust_http_cache initializer

--- config/initializers/bust_http_cache.rb
+++ config/initializers/bust_http_cache.rb
@@ -1,1 +1,1 @@
-ENV["ETAG_VERSION_ID"] = Time.now # TODO: change value to release version if there are several servers
+ENV["ETAG_VERSION_ID"] = Time.now.to_s # TODO: change value to release version if there are several servers

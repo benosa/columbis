cfc6c1acf27546616cde57034b255a04b23ae8c5
eddiefisher <eddifisher@bk.ru>
2013-07-29 00:22:36 +0400

    add phone migration v2

---
+++ db/migrate/20130726100500_add_phone_number_and_notes_to_tourist.rb
@@ -0,0 +1,32 @@
+class AddPhoneNumberAndNotesToTourist < ActiveRecord::Migration
+  def up
+    add_column :tourists, :note, :text
+    
+    Tourist.reset_column_information
+    
+    phones = Tourist.where('length(phone_number) notnull')
+    phones.each do |phone|
+      phone.update_attributes({ note: phone.phone_number, phone_number: nil })
+    end
+
+    notes = Tourist.where('length(note) > 8')
+    notes.each do |note|
+      puts "======> #{note.note}"
+      normal_phone = normalize_phone(note.note)
+      puts "normal phone: #{normal_phone}"
+      if normal_phone.length == 11
+        a = note.update_attributes({ phone_number: normal_phone, note: nil })
+        puts a.to_yaml
+      end
+      
+    end
+  end
+  
+  def down
+    remove_column :tourists, :note
+  end
+  
+  def normalize_phone number
+    number.gsub!(/\D/, '') || number
+  end
+end

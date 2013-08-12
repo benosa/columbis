0b2a8c517c017a454a39ac7278433c88e971852d
zaMmer <alexzammer@gmail.com>
2013-08-12 19:30:16 +0300

    [#135] Fix tourist_stat migration

--- db/migrate/20130621181321_correct_statistic_column.rb
+++ db/migrate/20130621181321_correct_statistic_column.rb
@@ -1,6 +1,8 @@
 class CorrectStatisticColumn < ActiveRecord::Migration
 
   def up
+    # Return old values for tourist_stat dropdown, because of it's Mistral requirement, maybe temporary.
+
     # values = [
     #   {name: I18n.t('intervals.channels.internet'), like_as: ["интернет", "инет", "инэт", "интернэт", "интэрнэт", "веб", "вэб"], stat: 0, old: []},
     #   {name: I18n.t('intervals.channels.recommendations'), like_as: ["рекоменд", "агент", "агенcт", "компания", "ооо", "оао"], stat: 0, old: []},
@@ -42,6 +44,26 @@ class CorrectStatisticColumn < ActiveRecord::Migration
     # DropdownValue.where(:list => "tourist_stat").delete_all
 
     # logger.close
+
+    # Change some values to common in tourist_stat list
+    %w(Интернет Знакомый).each do |value|
+      DropdownValue.where(:list => "tourist_stat", :value => value).update_all(:common => true, :company_id => nil)
+    end
+    DropdownValue.where(:list => "tourist_stat", :value => 'Реклама ТВ').update_all(:common => true, :company_id => nil, :value => 'Телевизор')
+
+    # Remove duplicate common values in tourist_stat list
+    %w(Интернет Знакомый Телевизор).each do |value|
+      ddvs = DropdownValue.where(:list => "tourist_stat", :value => value, :common => true).order(:id).all
+      if ddvs.length > 1
+        ddvs.delete_at(0)
+        ddvs.each{ |ddv| ddv.delete }
+      end
+    end
+
+    # Create new common values in tourist_stat list
+    %w(Рекомендации Клиенты Журналы/Газеты Вывеска Прочее).each do |value|
+      DropdownValue.create(:list => "tourist_stat", :common => true, :value => value)
+    end
   end
 
   def down

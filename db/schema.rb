40da2c085c295b0d74ad50b275004058bfd198e1
velez <nicegg@mail.ru>
2013-08-26 12:46:40 +0300

    [#131] Copy id to num in claims

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130826084926) do
+ActiveRecord::Schema.define(:version => 20130826090836) do
 
   create_table "addresses", :force => true do |t|
     t.integer  "addressable_id"

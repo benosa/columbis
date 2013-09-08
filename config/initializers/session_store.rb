8c00b3bdda38d305f1deae929fa231d74e70e800
zaMmer <alexzammer@gmail.com>
2013-09-08 23:49:48 +0300

    [#150] Fix domain for session store in development

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,9 @@
 # -*- encoding : utf-8 -*-
 # Be sure to restart your server when you modify this file.
 
-Tourism::Application.config.session_store :cookie_store, :key => CONFIG[:session_key] || '_columbis_session', :domain => CONFIG[:domain] || :all
+Tourism::Application.config.session_store :cookie_store,
+	:key => CONFIG[:session_key] || '_columbis_session',
+	:domain => CONFIG[:domain] unless Rails.env.development?
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

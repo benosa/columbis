70aa2c15920e0468c6d20a133dd828d816c6feab
velez <nicegg@mail.ru>
2013-09-19 13:33:26 +0300

    [#163] session, registration with json

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -17,15 +17,29 @@ class RegistrationsController < Devise::RegistrationsController
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
         sign_up(resource_name, resource)
-        return render :json => {:success => true}
+        respond_to do |format|
+          format.json { render :json => {:success => true} }
+          format.html { respond_with resource, :location => after_sign_up_path_for(resource) }
+        end
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
         expire_session_data_after_sign_in!
-        return render :json => {:success => true}
+        respond_to do |format|
+          format.json { render :json => {:success => true} }
+          format.html { respond_with resource, :location => after_inactive_sign_up_path_for(resource) }
+        end
       end
     else
       clean_up_passwords resource
-      return render :json => {:success => false}
+      errors = {}
+      resource.errors.messages.each do |key, value|
+        errors[key] = resource.errors.full_message(key,value[0])
+      end
+
+      respond_to do |format|
+        format.json { render :json => {:success => false, :errors => errors } }
+        format.html { respond_with resource }
+      end
     end
   end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,29 @@
 class SessionsController < Devise::SessionsController
-  respond_to :html, :json
 
   def create
-     self.resource = warden.authenticate!(auth_options)
-     set_flash_message(:notice, :signed_in) if is_navigational_format?
-     sign_in(resource_name, resource)
-     # Rails.logger.debug "@session.errors: #{resource.inspect}"
-     respond_to do |format|
-       format.html {  respond_with resource, :location => after_sign_in_path_for(resource) }
-       format.json { render :json => {:success => true} }
-     end
+    respond_to do |format|
+      format.json do
+        resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
+        sign_in_and_redirect(resource_name, resource)
+      end
+
+      format.html do
+        self.resource = warden.authenticate!(auth_options)
+        set_flash_message(:notice, :signed_in) if is_navigational_format?
+        sign_in(resource_name, resource)
+        respond_with resource, :location => after_sign_in_path_for(resource)
+      end
+    end
   end
 
-  def failure
-    render json: { success: false, errors: ['Login information is incorrect, please try again'] }
+  def sign_in_and_redirect(resource_or_scope, resource=nil)
+    scope = Devise::Mapping.find_scope!(resource_or_scope)
+    resource ||= resource_or_scope
+    sign_in(scope, resource) unless warden.user(scope) == resource
+    render :json => {:success => true, name: resource.first_name.to_s + ' ' + resource.last_name.to_s}
+  end
+
+  def failure()
+    return render :json => {:success => false, :errors => I18n.t('devise.failure.invalid') }
   end
 end

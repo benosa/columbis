847c1016e6fa64a6d87915eaf767848f2989385a
TOTOIIIUr <totoiiie4ka@gmail.com>
2013-09-25 16:58:57 +0400

    [#180] Fix mails. Tests.

--- spec/mailers/mailer_spec.rb
+++ spec/mailers/mailer_spec.rb
@@ -1,21 +1,41 @@
 require 'spec_helper'
 
-describe 'confirmation' do
+describe 'Mailer' do
   include EmailSpec::Helpers
   include EmailSpec::Matchers
 
-  before do
-    @user = FactoryGirl.create :user
-  end
+  describe "if user create" do
 
-  subject { Mailer.confirmation_instructions(@user) }
+    before do
+      @user = FactoryGirl.create(:admin)
+    end
 
-  describe "Email should have" do
-    it { should deliver_to @user.email }
-    it { should have_body_text(/#{@user.first_name}/) }
-    it { should have_body_text(/#{@user.last_name}/) }
-    it { should have_body_text(/#{@user.login}/) }
-    it { should have_body_text(/#{@user.password}/) }
-    it { should have_body_text(/#{@user.confirmation_token}/) }
+    context "should create confirmation mail" do
+      subject { Mailer.confirmation_instructions(@user) }
+      it { should deliver_to @user.email }
+      it { should have_body_text(/#{@user.first_name}/) }
+      it { should have_body_text(/#{@user.last_name}/) }
+      it { should have_body_text(/#{@user.login}/) }
+      it { should have_body_text(/#{@user.password}/) }
+      it { should have_body_text(/#{@user.confirmation_token}/) }
+    end
+
+    context "should create mail to support about user" do
+      subject { Mailer.user_was_created(@user) }
+      it { should deliver_to CONFIG[:support_email] }
+      it { should have_body_text(/#{@user.login}/) }
+    end
+
+    context "should create mail to support about office" do
+      subject { Mailer.office_was_created(@user.office) }
+      it { should deliver_to CONFIG[:support_email] }
+      it { should have_body_text(/#{@user.office.try(:name)}/) }
+    end
+
+    context "should create mail to support about compay" do
+      subject { Mailer.company_was_created(@user.company) }
+      it { should deliver_to CONFIG[:support_email] }
+      it { should have_body_text(/#{@user.company.try(:name)}/) }
+    end
   end
 end

6f3258bb0d77cfedb24220fa9fa77bce3c662819
velez <nicegg@mail.ru>
2013-10-23 12:08:36 +0300

    [#211] instructions download, test

--- spec/features/requests/application_spec.rb
+++ spec/features/requests/application_spec.rb
@@ -9,3 +9,13 @@ describe "SSL tests:" do
     current_url.split(':')[0].should == (CONFIG[:force_ssl] ? "https" : "http")
   end
 end
+
+describe "download tests:" do
+  #include ActionView::Helpers
+
+  it "protocol should be https" do
+    visit root_url + '/instructions.pdf'
+    page.response_headers['Content-Type'].should eq "application/pdf"
+  end
+end
+

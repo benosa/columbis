ec73ed12cb0d217a157d4a06b73e30de2280e5f1
TOTOIIIUr <totoiiie4ka@gmail.com>
2013-11-07 13:43:45 +0400

    [#95] Add colors to exporting spreadsheet document

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -40,9 +40,11 @@ class ClaimsController < ApplicationController
     inluded_tables = [:user, :office, :operator, :country, :city, :applicant, :dependents, :assistant]
     @totals = Claim.accessible_by(current_ability).includes(inluded_tables)
 
-    @tourists = Tourist.accessible_by(current_ability).includes(:address)
+    @tourists = Tourist.accessible_by(current_ability).includes([:address, :user])
 
     @clients = Tourist.accessible_by(current_ability).includes(:address).potentials
+
+    @managers = User.where(:company_id => current_company.id)
 
     respond_to do |format|
       format.xls { render "claims" }

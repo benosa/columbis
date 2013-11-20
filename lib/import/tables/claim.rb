bfa38ef5d464549d9d80fefd33c527c55b415dae
TOTOIIIUr <totoiiie4ka@gmail.com>
2013-11-20 17:06:37 +0400

    [#213] Create all checks

--- lib/import/tables/claim.rb
+++ lib/import/tables/claim.rb
@@ -29,8 +29,8 @@ module Import
           :primary_currency_price => {:type => 'Float',  :may_nil => true,  :value => nil},
           :operator_price         => {:type => 'Float',  :may_nil => true,  :value => nil},
           :operator_maturity      => {:type => 'Date',   :may_nil => true,  :value => nil},
-          :operator_paided        => {:type => 'Float',  :may_nil => true,  :value => nil},
-          :paided                 => {:type => 'Float',  :may_nil => true,  :value => nil},
+          :operator_paid          => {:type => 'Float',  :may_nil => true,  :value => nil},
+          :tourist_advance        => {:type => 'Float',  :may_nil => true,  :value => nil},
           :documents_status       => {:type => 'String', :may_nil => false, :value => 'Не готовы'},
           :docs_note              => {:type => 'String', :may_nil => true,  :value => ''},
           :check_date             => {:type => 'Date',   :may_nil => false, :value => Time.zone.now}
@@ -51,6 +51,8 @@ module Import
           if data_row
             claim = create_claim(data_row, company)
             if claim.save
+              # need add flights
+              # need add operator
               # logger need
             else
               # logger need
@@ -68,6 +70,7 @@ module Import
           if data_row
             # logger need
             fields_check(data_row, company)
+            check_for_nil(data_row)
           else
             # logger need
             false
@@ -90,53 +93,79 @@ module Import
 
         def fields_check(row, company)
           row.each do |field|
-            if private_instance_methods.include?("#{field[0]}_check") && !send("#{field[0]}_check", row, company)
-              # logger need
-              return false
+            if private_instance_methods.include?("#{field[0]}_check")
+              send("#{field[0]}_check", row, company)
             end
           end
+        end
+
+        def check_for_nil(row)
+          row.each do |field|
+            return false if row[field[0]][:value].nil? && !row[field[0]][:may_nil]
+          end
+          true
         end
 
         def user_check(row, company)
-          user = company.users.where(:login => row[:user]).first
-          check_field(row[:user], user.try(:id))
+          user = company.users.where(:login => row[:user][:value]).first
+          row[:user][:value] = user.try(:id)
         end
 
         def office_check(row, company)
-          office = company.offices.where(:name => row[:office]).first
+          office = company.offices.where(:name => row[:office][:value]).first
           office = company.offices.first if office.nil?
-          check_field(row[:office], office.try(:id))
+          row[:office][:value] = office.try(:id)
         end
 
         def full_name_check(row, company)
-          tourist = Tourist.where(:company_id => company.id)
-            .where("passport_series = '#{row[:passport_series][:value]}'")
-            .where("passport_number = '#{row[:passport_number][:value]}'")
-            .first unless (row[:passport_series][:value] == nil && row[:passport_number][:value] == nil)
-          unless tourist
-            tourist = Tourist.new do |t|
-              t.full_name = row.delete(:full_name)
-              t.passport_series = row.delete(:passport_series)
-              t.passport_number = row.delete(:passport_number)
-              t.passport_valid_until = row.delete(:passport_valid_until)
-              t.date_of_birth = row.delete(:date_of_birth)
-              t.email = row.delete(:email)
-              t.phone_number = row.delete(:telephone)
+          tourist =
+            if row[:passport_series][:value] != nil && row[:passport_number][:value] != nil
+              Tourist.where(:company_id => company.id)
+                .where("passport_series = '#{row[:passport_series][:value]}'")
+                .where("passport_number = '#{row[:passport_number][:value]}'")
+                .first
             end
-          end
+          tourist =
+            unless tourist
+              Tourist.new do |t|
+                t.full_name = row.delete(:full_name)[:value]
+                t.passport_series = row.delete(:passport_series)[:value]
+                t.passport_number = row.delete(:passport_number)[:value]
+                t.passport_valid_until = row.delete(:passport_valid_until)[:value]
+                t.date_of_birth = row.delete(:date_of_birth)[:value]
+                t.email = row.delete(:email)[:value]
+                t.phone_number = row.delete(:telephone)[:value]
+              end
+            end
           if tourist.id || tourist.valid?
-            row.merge!({:tourist => tourist.attributes})
+            row.merge!({:tourist => {:value => tourist.attributes, :may_nil => false}})
           else
-            # logger need
-            false
+            row.merge!({:tourist => {:value => nil, :may_nil => false}})
           end
         end
 
         def operator_check(row, company)
-        end
-
-        def airport_back_check(row, company)
-          check_field(row[:airport_back], row[:airport_back][:value])
+          operator =
+            if row[:operator_number][:value] != nil && row[:operator_series][:value] != nil
+              Operator.where(:company_id => company.id)
+                .where("operator_number = '#{row[:operator_number][:value]}'")
+                .where("operator_series = '#{row[:operator_series][:value]}'")
+                .first
+            end
+          operator =
+            unless operator
+              Operator.new do |o|
+                o.name = row.delete(:operator)[:value]
+                o.operator_number = row.delete(:operator_number)[:value]
+                o.operator_series = row.delete(:operator_series)[:value]
+                o.operator_confirmation = row.delete(:operator_confirmation)[:value]
+              end
+            end
+          if operator.id || operator.valid?
+            row.merge!({:operator => {:value => operator.attributes, :may_nil => false}})
+          else
+            row.merge!({:operator => {:value => nil, :may_nil => false}})
+          end
         end
 
         def visa_check(row, company)
@@ -145,44 +174,56 @@ module Import
             visa_statuses.merge!({ I18n.t(".claims.visa_statuses.#{status}") => status })
           end
           if visa_statuses.keys.include?(row[:visa][:value])
-            row[:visa][:value] = visa_statuses[:value]
+            row[:visa][:value] = visa_statuses[row[:visa][:value]]
           else
-            # logger need
-            false
+            row[:visa][:value] = nil
           end
         end
 
-        def visa_check_ckeck(row, company)
-          check_field(row[:visa_check], row[:visa_check][:value])
-        end
-
-        def check_field(field, value)
-          if value.nil?
-            false || field[:may_nil]
+        def documents_status_check(row, company)
+          documents_statuses = {}
+          Claim::DOCUMENTS_STATUSES.each do |status|
+            documents_statuses.merge!({ I18n.t(".claims.documents_statuses.#{status}") => status })
+          end
+          if documents_statuses.keys.include?(row[:documents_status][:value])
+            row[:documents_status][:value] = documents_statuses[row[:visa][:value]]
           else
-            field[:value] = value
+            row[:documents_status][:value] = nil
           end
         end
+
+        # def check_field(field, value)
+        #   if value.nil?
+        #     false || field[:may_nil]
+        #   else
+        #     field[:value] = value
+        #   end
+        # end
 
         def create_claim(row, company)
           claim = Claim.new do |c|
             # relations
             c.company_id = company
-            c.user_id = row[:user]
-            c.office_id = row["office"]
+            c.user_id = row[:user][:value]
+            c.office_id = row["office"][:value]
             # common
-            c.reservation_date = row[:date]
-            c.check_date = row[:check_date]
-            c.tourist_stat = row[:promotion]
-            c.arrival_date = row[:arrival_date]
-            c.departure_date = row[:departure_date]
-            c.country = row[:country]
+            c.reservation_date = row[:date][:value]
+            c.check_date = row[:check_date][:value]
+            c.tourist_stat = row[:promotion][:value]
+            c.arrival_date = row[:arrival_date][:value]
+            c.departure_date = row[:departure_date][:value]
+            c.country = row[:country][:value]
             # tourist
-            c.applicant_attributes = row[:tourist]
-            # need add flights
-            c.visa = row[:visa]
-            c.visa_check = row[:visa_check]
-            c.applicant_attributes = row[:tourist]
+            c.applicant_attributes = row[:tourist][:value]
+            c.visa = row[:visa][:value]
+            c.visa_check = row[:visa_check][:value]
+            c.applicant_attributes = row[:tourist][:value]
+            c.primary_currency_price = row[:primary_currency_price][:value]
+            c.operator_price = row[:operator_price][:value]
+            c.operator_maturity = row[:operator_maturity][:value]
+            c.operator_paid = row[:operator_paid][:value]
+            c.docs_note = row[:docs_note][:value]
+            c.tourist_advance = row[:tourist_advance][:value]
           end
         end
       end

418179076e05bd3c47f542cfe697456ae5af1884
zaMmer <alexzammer@gmail.com>
2014-03-07 04:00:32 +0200

    Fix return_call flag in claim

--- db/migrate/20131223082841_add_return_call_to_claims.rb
+++ db/migrate/20131223082841_add_return_call_to_claims.rb
@@ -1,5 +1,5 @@
 class AddReturnCallToClaims < ActiveRecord::Migration
   def change
-  	add_column :claims, :return_call, :boolean, :default => false
+  	add_column :claims, :return_call, :boolean, :default => true, :null => false
   end
 end

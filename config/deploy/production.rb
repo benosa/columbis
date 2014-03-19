aeb3988d16d9542f5694d9e1a883aa29f774f1c0
zaMmer <alexzammer@gmail.com>
2014-03-20 00:26:07 +0200

    Change production deploy to new server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,13 +4,13 @@ set :branch, "production"
 set :application, "columbis"
 set(:deploy_to) { "/opt/apps/#{application}" }
 
-set :domain, "columbis.ru"
+set :domain, "188.226.190.90"
 
 role :app, domain
 role :web, domain
 role :db,  domain, :primary => true
 
-set :rvm_ruby_string, "2.0.0-p0@tourism"
+set :rvm_ruby_string, "2.0.0-p451@columbis"
 set :rvm_type, :user
 
 set :rails_env, "production"

73f2bb522693c9366bd8b6430ca8068befe048b4
zaMmer <alexzammer@gmail.com>
2014-04-07 01:24:15 +0400

    Fix rvm_ruby_string in cron schedule, add thinking_sphinx:reindex task to schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,14 @@
 set :output, File.expand_path('../../log/cron.log', __FILE__)
-set :rvm_ruby_string, '2.0.0@tourism'
+set :rvm_ruby_string, '2.0.0-p451@columbis'
 
 job_type :rake_with_rvm, 'rvm use :rvm_ruby_string && cd :path && RAILS_ENV=:environment bundle exec rake :task --trace :output'
 
 every 1.day, :at => '1:17 am', :roles => [:app] do
   rake_with_rvm 'claims:expire_active_cache'
+end
+
+every 1.day, :at => '1:47 am', :roles => [:app] do
+  rake_with_rvm 'thinking_sphinx:reindex'
 end
 
 every 1.day, :at => '2:12 am', :roles => [:app] do

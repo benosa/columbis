e861957f98b8c9310f39cb21f210272814eb6c37
zaMmer <alexzammer@gmail.com>
2014-04-22 18:21:41 +0400

    Fix migration

--- db/migrate/20140416085508_fill_company_operators.rb
+++ db/migrate/20140416085508_fill_company_operators.rb
@@ -3,6 +3,7 @@ class FillCompanyOperators < ActiveRecord::Migration
     Company.where(active: true).find_each do |company|
       if company.is_active?
         used_operators = CompanyOperator.where(company_id: company.id).pluck(:operator_id)
+        used_operators << 0 if used_operators.empty?
         Operator.where("operators.id NOT IN (?)", used_operators).where(company_id: company.id, common: false).find_each do |operator|
           CompanyOperator.create(company_id: company.id, operator_id: operator.id)
           used_operators << operator.id

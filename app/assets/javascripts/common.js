7412c1be1d8ebd2f55b8b120055c324ed869ed3b
velez <nicegg@mail.ru>
2014-07-31 11:25:35 +0400

    [#344] export notification final

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,41 +1,53 @@
 var validNavigation = false;
 
-function setCookie(name, value, days) {
-    if (days) {
-    var date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    var expires = '; expires=' + date.toGMTString()
-    } else var expires = '';
-    document.cookie = name + '=' + value + expires + '; path=/;';
+function setCookie(name, value) {
+    var currentDate = new Date();
+    expirationDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()+1, 0, 0, 0);
+    $.cookie(name, value, {expires: expirationDate});
   }
 function getCookie(name) {
   var nameEQ = name + '=';
   var ca = document.cookie.split(';');
   for (var i = 0; i < ca.length; i++) {
-  var c = ca[i];
-  while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-  if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length)
+    var c = ca[i];
+    while (c.charAt(0) == ' ')
+      c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0)
+      return c.substring(nameEQ.length, c.length)
   }
   return null
 }
 
-function wireUpEvents() {
+function export_notification_f() {
+  if (!validNavigation) {
+    var notificated = getCookie('export_notification');
+    var message = $('body').data('export_n_message');
 
-  $(window).on('beforeunload', function() {
-    if (!validNavigation) {
-      var notificated = getCookie('export_notification');
-      var message = $('body').data('export_n_message');
-      if (notificated != '1') {
-        setCookie('export_notification', '1', 1);
-        if (navigator.userAgent.search(/Firefox/) > -1) {
-          alert(message);
-          return 1;
-        } else {
-          return message;
-        }
+    if (notificated != '1') {
+      setCookie('export_notification', 1);
+      if (navigator.userAgent.search(/Firefox/) > -1) {
+        alert(message);
+        return 1;
+      } else {
+        return message;
       }
     }
-  });
+  }
+}
+
+function export_notification_exit() {
+  var notificated = getCookie('export_notification');
+  var message = $('body').data('export_n_message');
+  if (notificated != '1') {
+    setCookie('export_notification', 1);
+    alert(message);
+    return false;
+  }
+}
+
+function wireUpEvents() {
+
+  $(window).on('beforeunload', export_notification_f);
 
   // Attach the event keypress to exclude the F5 refresh
   $(document).bind('keypress', function(e) {
@@ -64,6 +76,7 @@ function wireUpEvents() {
 $(document).ready(function() {
   if ($('body').data('export_notification') == true) {
     wireUpEvents();
+    $("#logout_link").bind("click", export_notification_exit);
   }
 });
 
